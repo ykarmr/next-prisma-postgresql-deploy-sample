7c5d9733e4fc21d013220dcde9780198f4cda6eb
ykarmr <yuki.arimura0508@gmail.coml>
2021-07-18 15:21:50 +0900

    fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,22 +1,20 @@
-// lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const prismaClientPropertyName = `__prevent-name-collision__prisma`;
-type GlobalThisWithPrismaClient = typeof globalThis & {
-	[prismaClientPropertyName]: PrismaClient;
-};
+let prisma;
 
-const getPrismaClient = () => {
-	if (process.env.NODE_ENV === `production`) {
-		return new PrismaClient();
-	} else {
-		const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
-		if (!newGlobalThis[prismaClientPropertyName]) {
-			newGlobalThis[prismaClientPropertyName] = new PrismaClient();
-		}
-		return newGlobalThis[prismaClientPropertyName];
-	}
-};
-const prisma= getPrismaClient();
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  // @ts-ignore
+  if (!global.prisma) {
+    // @ts-ignore
+    global.prisma = new PrismaClient();
+  }
 
-export default prisma
+  // @ts-ignore
+  prisma = global.prisma;
+}
+
+export default prisma;
+
+export * from "@prisma/client";
